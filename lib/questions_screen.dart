b0291ce517638bfdb2db32d47acf6a3035cc37de
ssangttuce <sanghoo1023@gmail.com>
2024-02-27 17:32:10 +0900

    question data and button widget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,36 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "The question...",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: "answer 1",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "answer 2",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "answer 3",
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: "answer 4",
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
